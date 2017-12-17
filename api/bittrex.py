c925c5c579a18610384680b41a74c70f80cacae3
Ting Yao Fam <tingyao7798@gmail.com>
2017-12-17 18:17:54 +0800

    refactor get wallet value method

--- api/bittrex.py
+++ api/bittrex.py
@@ -19,7 +19,7 @@ class Bittrex:
         self.__api_secret = api_secret
 
     def _get_balance(self):
-        """ method to get bittrex wallet balances
+        """ method to get bittrex balances
         Returns:
             wallet: list of dict e.g. 
                   [{'Available': 8011.86885714,
@@ -29,45 +29,56 @@ class Bittrex:
                     'Pending': 0.0}]
         """
         logger = logging.getLogger(__name__)
-        logger.info("Retrieving Bittrex account balances...")
+        logger.debug("Retrieving Bittrex account balances...")
 
         client = bittrex.Bittrex(self.__api_key, self.__api_secret)
         resp = client.get_balances()
 
         if resp['success']:
-            logger.info(resp['result'])
+            logger.debug(resp['result'])
             wallet = resp['result']
             return wallet
         else:
             logging.error("Failed to get Bittrex account balances!")
 
-    def get_wallet_value(self):
-        """ method to
+    def _format_wallet(self):
+        """ method to format wallet
         Args:
             wallet: list of dict
         Returns:
-            wallet_value: float
+            wallet_dict: dict: float e.g.{'iot':222}
         """
-        # get wallet balance
         wallet = self._get_balance()
 
         # init wallet dict
         wallet_dict = {}
         for coin in wallet:
             if coin['Balance'] > 0:
-                wallet_dict.update({coin['Currency']:coin['Balance']})
+                wallet_dict.update({coin['Currency'].upper():coin['Balance']})
+
+        return wallet_dict
+
+    def get_wallet_value(self):
+        """ method to get wallet value
+        Args:
+            wallet: list of dict
+        Returns:
+            wallet_value: float
+        """
+        # get wallet balance
+        wallet = self. _format_wallet()
 
         # get latest prices from coinmarketcap
         cmc_client = Coinmarketcap()
         ticker_prices = cmc_client.get_ticker_prices()
 
-        total_value = 0.
+        wallet_value = 0.
 
-        for coin, amt in wallet_dict.items():
+        for coin, amt in wallet.items():
             price_usd = float(ticker_prices[coin])
-            total_value = total_value + (amt * price_usd)
+            wallet_value = wallet_value + (amt * price_usd)
 
-        return total_value
+        return wallet_value
 
 
 
